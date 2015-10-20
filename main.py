482c0785c8e711d9ca7f9ab343d43d4b1080c027
tsh <dr.tallin@gmail.com>
2015-10-20 16:55:34 +0300

    parse push event

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import requests, json, getpass
-"""
-https://api.github.com/repos/dgladkov/noseyboy/commits
-"""
 
-github_url = "https://api.github.com/repos/dgladkov/noseyboy/commits"
+intresting_events = ['PushEvent', 'IssuesEvent']
+template = """
+Name:       {author}
+Url:        {url}
+Message:    {message}
+   """
 
+def parse_push_event(event):
+    print_params = {'author': event['actor']['login'],
+                    'url': event['payload']['commits'][0]['url'],
+                    'message': event['payload']['commits'][0]['message']}
+    return print_params
+
+github_url = "https://api.github.com/users/{user}/events"
 
 if __name__ == '__main__':
     usr = input('Username: ')
     pwd = getpass.getpass()
-    r = requests.get(github_url, auth=(usr, pwd))
-    print (r.json()[0])
+    req = requests.get(github_url.format(user=usr), auth=(usr, pwd))
+    events = req.json()
+    filtered_events = filter(lambda d: d['type'] in intresting_events, events)
+    for event in filtered_events:
+        if event['type'] == 'PushEvent':
+            print(event)
+            print(template.format(**parse_push_event(event)))
