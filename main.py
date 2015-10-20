0c5089f1b81cc7fa591abeb0ac87501bcb1bddcc
tsh <dr.tallin@gmail.com>
2015-10-20 16:59:27 +0300

    added todo

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import requests, json, getpass
 
 intresting_events = ['PushEvent', 'IssuesEvent']
+#TODO: filter by repositores only private?
+#TODO: print date/time
+
 template = """
 Name:       {author}
 Url:        {url}
 Message:    {message}
+Date:       {date}: {time}
    """
 
 def parse_push_event(event):
-    print_params = {'author': event['actor']['login'],
-                    'url': event['payload']['commits'][0]['url'],
+    print_params = {'author': event['actor']['login'],  #TODO: use loop to parse commit
+                    'url': event['payload']['commits'][0]['url'], #TODO: use sha to construct web based commit
                     'message': event['payload']['commits'][0]['message']}
     return print_params
+
+def parse_issue_event(event):
+    # TODO: me
+    pass
 
 github_url = "https://api.github.com/users/{user}/events"
 
