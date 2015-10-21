13f95978188c1b0d53599e20e2c1209ece818913
tsh <dr.tallin@gmail.com>
2015-10-21 16:58:30 +0300

    filter by private repo, datetime

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-import requests, json, getpass
+import requests, json, getpass, dateutil.parser
 
 intresting_events = ['PushEvent', 'IssuesEvent']
-#TODO: filter by repositores only private?
-#TODO: print date/time
 
 template = """
 Name:       {author}
 Url:        {url}
-Message:    {message}
 Date:       {date}: {time}
+Message:    {message}
    """
 
 def parse_push_event(event):
+    date_time = dateutil.parser.parse(event['created_at'])
     print_params = {'author': event['actor']['login'],  #TODO: use loop to parse commit
                     'url': event['payload']['commits'][0]['url'], #TODO: use sha to construct web based commit
-                    'message': event['payload']['commits'][0]['message']}
+                    'message': event['payload']['commits'][0]['message'],
+                    'date': date_time.date(),
+                    'time': date_time.time()}
     return print_params
 
 def parse_issue_event(event):
@@ -28,8 +29,9 @@ if __name__ == '__main__':
     pwd = getpass.getpass()
     req = requests.get(github_url.format(user=usr), auth=(usr, pwd))
     events = req.json()
-    filtered_events = filter(lambda d: d['type'] in intresting_events, events)
-    for event in filtered_events:
+    filtered_type_events = filter(lambda d: d['type'] in intresting_events, events)
+    filtered_visibility_events = filter(lambda d: d['public'] is False, filtered_type_events)
+    for event in filtered_visibility_events:
         if event['type'] == 'PushEvent':
             print(event)
             print(template.format(**parse_push_event(event)))
