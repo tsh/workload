f8b158ab0fc6cc4f0c3b296ac8ff7e23933b66ad
tsh <dr.tallin@gmail.com>
2015-12-02 17:17:58 +0200

    add record model

--- api.py
+++ api.py
@@ -1,6 +1,12 @@
 import os
+
+from datetime import datetime
+from dateutil import parser as datetime_parser
+from dateutil.tz import tzutc
 from flask import Flask, url_for, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
+
+from .utils import url_parse
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, '../data.sqlite')
@@ -19,6 +25,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
+    records = db.relationship('Record', backref='users', lazy='dynamic')
 
     def get_url(self):
         return url_for('get_user', id=self.id, _external=True)
@@ -26,7 +33,8 @@ class User(db.Model):
     def export_data(self):
         return {
             'self_url': self.get_url(),
-            'name': self.name
+            'name': self.name,
+            'records_url': url_for('get_user_records', id=self.id, _external=True)
         }
 
     def import_data(self, data):
@@ -36,6 +44,31 @@ class User(db.Model):
             raise ValidationError('Invalid user: missing ' + e.args[0])
         return self
 
+
+class Record(db.Model):
+    __tablename__ = 'records'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # TODO: add start, end date
+    timestamp = db.Column(db.DateTime, default=datetime.now)
+
+    def get_url(self):
+        return url_for('get_record', id=self.id, _external=True)
+
+    def export_data(self):
+        return {
+            'self_url': self.get_url(),
+            'description': self.description
+        }
+
+    def import_data(self, data):
+        try:
+            self.description = self.data['description']
+        except KeyError as e:
+            raise ValidationError('Invalid record: ' + e.args[0])
+        return self
+    
 
 @app.route('/users/', methods=['GET'])
 def get_users():
